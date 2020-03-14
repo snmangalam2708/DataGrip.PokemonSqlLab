1a6cbddf05a36a33bf4967cb36161242f188644a
sandeep <snaraya@zipcodes-MBP-10.fios-router.home>
2020-03-14 11:19:19 -0400

    Complete Pokeman lab

--- Answer_Queries.sql
+++ Answer_Queries.sql
@@ -20,3 +20,18 @@ SELECT primary_type, COUNT(*) FROM pokemons GROUP BY primary_type;
 SELECT trainers.trainername, COUNT(*) FROM pokemon_trainer, trainers WHERE pokelevel = 100 AND pokemon_trainer.trainerID = trainers.trainerID GROUP BY trainers.trainerID;
 SELECT pokemon_id FROM pokemon_trainer GROUP BY pokemon_id HAVING COUNT(*) = 1;
 
+/***************************PART 4**********************************/
+
+/*Part 4 - HIGHEST BASED ON ATT, SPA, SPD TOTAL.*/
+SELECT p.name AS pokemon_name, t.trainername AS trainer_name, pt.pokelevel AS level,
+ptype.name AS type, stype.name AS sub_type, (attack + spatk + speed) AS AttSpaSpd
+FROM pokemons AS p
+JOIN pokemon_trainer AS pt
+ON p.id = pt.pokemon_id
+JOIN trainers AS t
+ON pt.trainerID = t.trainerID
+RIGHT JOIN types AS ptype
+ON p.primary_type = ptype.id
+LEFT JOIN types AS stype
+ON p.secondary_type = stype.id
+ORDER BY AttSpaSpd DESC;
